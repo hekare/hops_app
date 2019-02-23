76ce14589cc3e1ffaf5a7ca92cdc327036f33736
Heikki Reijonen <reijonen@student.tut.fi>
2019-02-23 14:14:30 +0200

    muutoksia, jos toimisi

--- hops/settings.py
+++ hops/settings.py
@@ -10,8 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
-import json
+import os, json, django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
